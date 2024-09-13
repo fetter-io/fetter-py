f7d6021c69a1fe7120971074bc6e41d2fcbcdb7b
Christopher Ariza <ariza@flexatone.com>
2024-09-13 12:06:21 -0700

    preliminary interface with fetter-rs

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+// use std::env;
+use pyo3::prelude::*;
+use pyo3::types::PyList;
+use ::fetter::run_cli;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn run(args: Vec<String>) -> PyResult<()> {
+    // let args: Vec<String> = py_args.iter().map(|arg| arg.to_string()).collect();
+    // env::set_var("RUST_BACKTRACE", "1");
+    run_cli(args);
+    Ok(())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn fetter(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(run, m)?)?;
+    Ok(())
+}
