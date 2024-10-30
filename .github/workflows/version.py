396e25711c5d77c050adb5cc8754f63f2f792b36
Christopher Ariza <ariza@flexatone.com>
2024-10-30 15:42:13 -0700

    implement auto toml update

--- .github/workflows/version.py
+++ .github/workflows/version.py
@@ -1,4 +1,50 @@
 
+# much simplified version of https://github.com/astral-sh/ruff-pre-commit/blob/main/mirror.py
 
-if __name__ == '__main__':
-    print('update script')
+from subprocess import run
+import tomllib
+from pathlib import Path
+from urllib import request
+from io import BytesIO
+import json
+
+def get_lib_version() -> str:
+    with request.urlopen("https://crates.io/api/v1/crates/fetter") as response:
+        result = BytesIO(response.read())
+    return json.loads(result.read())['crate']['max_stable_version']
+
+def get_self_version() -> str:
+    with open(Path(__file__).parent.parent.parent / "Cargo.toml", 'rb') as f:
+        cargo = tomllib.load(f)
+        for k, v in cargo["dependencies"].items():
+            if k.startswith('fetter'):
+                return v
+
+def update_cargo_toml(self_version: str, lib_version: str):
+    with open('Cargo.toml') as f:
+        content = f.read()
+        content = content.replace(
+            f'fetter = "{self_version}"', f'"fetter = "{lib_version}"')
+        content = content.replace(
+            f'version = "{self_version}"', f'"version = "{lib_version}"')
+
+    with open('Cargo.toml', 'w') as f:
+        f.write(content)
+
+
+def main():
+    lib_version = get_lib_version()
+    self_version = get_self_version()
+    print(f"fetter-rs: {lib_version}, fetter-py: {self_version}")
+
+    if lib_version != self_version:
+        update_cargo_toml(self_version, lib_version)
+
+    #     run(["git", "commit", "-m", f"Mirror: {lib_version}", "-a"], check=True)
+    #     run(["git", "tag", f"v{lib_version}"], check=True)
+
+
+if __name__ == "__main__":
+    main()
+
+
