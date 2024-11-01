a995a3f7b3b8ab232c19fbb6801cf39dfbf57265
Christopher Ariza <ariza@flexatone.com>
2024-11-01 11:26:39 -0700

    removed cli error reporting

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,35 @@ use std::env;
 
 #[pyfunction]
 fn run(args: Vec<String>) -> PyResult<()> {
-    let _ = run_cli(args);
-    Ok(())
+    match run_cli(args) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("Error: {}", e)))
+        }
+    }
 }
+
+
+// #[pyfunction]
+// fn run_with_argv() -> PyResult<()> {
+//     // the first argument is Python, which must be removed
+//     let args: Vec<String> = env::args().skip(1).collect();
+//     let _ = run_cli(args);
+//     Ok(())
+// }
+
 
 #[pyfunction]
 fn run_with_argv() -> PyResult<()> {
     // the first argument is Python, which must be removed
     let args: Vec<String> = env::args().skip(1).collect();
-    let _ = run_cli(args);
-    Ok(())
+    match run_cli(args) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    }
 }
 
 #[pymodule]
