21c0d5487c1d2c0165575af35def869125ec00c9
Christopher Ariza <ariza@flexatone.com>
2024-11-08 13:04:38 -0800

    1.0.0 RC 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-// use std::env;
 use ::fetter::run_cli;
+use ::fetter::write_color;
+use std::io::stderr;
+
 use pyo3::prelude::*;
 use std::env;
 
@@ -14,25 +16,18 @@ fn run(args: Vec<String>) -> PyResult<()> {
     }
 }
 
-// #[pyfunction]
-// fn run_with_argv() -> PyResult<()> {
-//     // the first argument is Python, which must be removed
-//     let args: Vec<String> = env::args().skip(1).collect();
-//     let _ = run_cli(args);
-//     Ok(())
-// }
-
 #[pyfunction]
 fn run_with_argv() -> PyResult<()> {
     // the first argument is Python, which must be removed
     let args: Vec<String> = env::args().skip(1).collect();
-    match run_cli(args) {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            std::process::exit(1);
-        }
+    if let Err(e) = run_cli(args) {
+        let mut stderr = stderr();
+        write_color(&mut stderr, "#666666", "fetter ");
+        write_color(&mut stderr, "#cc0000", "Error: ");
+        eprintln!("{}", e);
+        std::process::exit(1);
     }
+    Ok(())
 }
 
 #[pymodule]
