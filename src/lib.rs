e5d6bdbb96116993e89bbdcb4af83ffd875110fb
Christopher Ariza <ariza@flexatone.com>
2025-03-25 16:19:51 -0700

    updated call to run_cli to pass client

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 use ::fetter::run_cli;
 use ::fetter::write_color;
+use ::fetter::UreqClientLive;
 use std::io::stderr;
+use std::sync::Arc;
 
 use pyo3::prelude::*;
 use std::env;
 
 #[pyfunction]
 fn run(args: Vec<String>) -> PyResult<()> {
-    match run_cli(args) {
+    let client = Arc::new(UreqClientLive);
+
+    match run_cli(args, client) {
         Ok(_) => Ok(()),
         Err(e) => Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
             "Error: {}",
@@ -20,7 +24,9 @@ fn run(args: Vec<String>) -> PyResult<()> {
 fn run_with_argv() -> PyResult<()> {
     // the first argument is Python, which must be removed
     let args: Vec<String> = env::args().skip(1).collect();
-    if let Err(e) = run_cli(args) {
+    let client = Arc::new(UreqClientLive);
+
+    if let Err(e) = run_cli(args, client) {
         let mut stderr = stderr();
         write_color(&mut stderr, "#666666", "fetter ");
         write_color(&mut stderr, "#cc0000", "Error: ");
